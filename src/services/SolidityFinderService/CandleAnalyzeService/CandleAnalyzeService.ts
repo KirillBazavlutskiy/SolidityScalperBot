2f09c08465aa3b476f61c548a4f9a51b1df7fef8
IvanPashchenko <100514008+PashchenkoIvan@users.noreply.github.com>
2024-01-23 22:49:11 +0200

    update

--- src/services/SolidityFinderService/CandleAnalyzeService/CandleAnalyzeService.ts
+++ src/services/SolidityFinderService/CandleAnalyzeService/CandleAnalyzeService.ts
@@ -1,7 +1,13 @@
-import {Binance, CandleChartInterval} from "binance-api-node";
+import {Binance, CandleChartInterval, CandleChartInterval_LT} from "binance-api-node";
+import {
+    SymbolDensityCoefficientInterface,
+    SymbolVolatilityInterface
+} from "./CalcCoefficientModels";
 
 export class CandleAnalyzeService {
     private static client: Binance;
+
+    private static coefficient: number;
 
     static SetBinanceClient = (client: Binance) => {
         this.client = client;
@@ -28,25 +34,28 @@ export class CandleAnalyzeService {
         }
     }
 
-    static CheckForAcceptablePriceChange = async (symbol: string, durationMinutes: number, acceptablePriceChange: number) => {
-        const priceChange = await this.GetPriceChange(symbol, durationMinutes);
+    static CheckForAcceptableAveragePriceChange = async (symbol: string, durationMinutes: number, acceptablePriceChange: number) => {
+        const priceChange = await this.GetAveragePriceChange(symbol, durationMinutes);
         return ({ access: Math.abs(priceChange)  <= acceptablePriceChange, priceChange: priceChange });
     }
 
-    static GetPriceChange = async (symbol: string, durationMinutes: number): Promise<number> =>  {
+    static GetAveragePriceChange = async (symbol: string, durationMinutes: number): Promise<number> => {
         const candles = await this.client.candles({
             symbol,
             interval: CandleChartInterval.ONE_MINUTE,
             limit: durationMinutes
         });
 
-        const lastCandles = candles.slice(-durationMinutes);
+        const sumOpenPrice = candles.reduce(
+            (sum, candle) => sum + parseFloat(candle.open),
+            0
+        )
+        const averageOpenPrice = sumOpenPrice / durationMinutes;
+        const lastCandle = candles[0];
 
-        const firstCandle = lastCandles[0];
-        const lastCandle = lastCandles[lastCandles.length - 1];
-
-        return ((parseFloat(lastCandle.close) - parseFloat(firstCandle.open)) / parseFloat(firstCandle.open)) * 100;
+        return ((parseFloat(lastCandle.close) - averageOpenPrice) / averageOpenPrice) * 100;
     }
+
 
     static GetVolumeOnPeriod = async (symbol: string, durationMinutes) => {
         try {
@@ -63,5 +72,37 @@ export class CandleAnalyzeService {
         } catch (e) {
             throw e;
         }
+    }
+
+    static getVolatility = async (symbol: string, interval: CandleChartInterval_LT = "1h", limit: number = 24) => {
+        const candlesData = await this.client.candles({symbol: symbol, interval: interval, limit: limit});
+        const closePrice_array = candlesData.map(candle => parseFloat(candle.close));
+
+        const priceChange = [];
+        for (let i = 0; i < closePrice_array.length; i++) {
+            const tempPrice_change = ((closePrice_array[i + 1] - closePrice_array[i]) / closePrice_array[i]) * 100;
+            priceChange.push(tempPrice_change)
+        }
+
+        const volatility = priceChange.reduce((a, b) => a + b, 0)
+
+        const resultObject: SymbolVolatilityInterface = {
+            symbol: symbol,
+            volatility: volatility
+        }
+
+        return resultObject
+    }
+
+    static calcCoefficient = async (symbol: string, interval: CandleChartInterval_LT = "1h", limit: number = 24) => {
+        const SymbolVolatilityObject = await this.getVolatility(symbol, interval, limit);
+
+        const resultObject: SymbolDensityCoefficientInterface = {
+            symbol: symbol,
+            coefficient: (SymbolVolatilityObject.volatility * this.coefficient) + 20,
+            volatility: SymbolVolatilityObject.volatility
+        }
+
+        return resultObject
     }
 }
