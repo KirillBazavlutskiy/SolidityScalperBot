7b6663f603bde58ce227f3f79643c33f33bbf5ac
KirillBazavlutskiy <bazik345@gmail.com>
2024-01-23 19:11:20 +0200

    GetAveragePriceChange func

--- src/services/SolidityFinderService/CandleAnalyzeService/CandleAnalyzeService.ts
+++ src/services/SolidityFinderService/CandleAnalyzeService/CandleAnalyzeService.ts
@@ -48,6 +48,29 @@ export class CandleAnalyzeService {
         return ((parseFloat(lastCandle.close) - parseFloat(firstCandle.open)) / parseFloat(firstCandle.open)) * 100;
     }
 
+    static CheckForAcceptableAveragePriceChange = async (symbol: string, durationMinutes: number, acceptablePriceChange: number) => {
+        const priceChange = await this.GetAveragePriceChange(symbol, durationMinutes);
+        return ({ access: Math.abs(priceChange)  <= acceptablePriceChange, priceChange: priceChange });
+    }
+
+    static GetAveragePriceChange = async (symbol: string, durationMinutes: number): Promise<number> => {
+        const candles = await this.client.candles({
+            symbol,
+            interval: CandleChartInterval.ONE_MINUTE,
+            limit: durationMinutes
+        });
+
+        const sumOpenPrice = candles.reduce(
+            (sum, candle) => sum + parseFloat(candle.open),
+            0
+        )
+        const averageOpenPrice = sumOpenPrice / durationMinutes;
+        const lastCandle = candles[0];
+
+        return ((parseFloat(lastCandle.close) - averageOpenPrice) / averageOpenPrice) * 100;
+    }
+
+
     static GetVolumeOnPeriod = async (symbol: string, durationMinutes) => {
         try {
             const candles = await this.client.candles({
