be841f63ba6b20b158e2ad18666f0c0c4f652345
IvanPashchenko <100514008+PashchenkoIvan@users.noreply.github.com>
2024-01-23 22:50:08 +0200

    add calcCoefficient and getVolatility in CandleAnalyzeService

---
+++ src/services/SolidityFinderService/CandleAnalyzeService/CalcCoefficientModels.ts
@@ -0,0 +1,10 @@
+export interface SymbolVolatilityInterface {
+    symbol: string,
+    volatility: number
+}
+
+export interface SymbolDensityCoefficientInterface {
+    symbol: string,
+    coefficient: number,
+    volatility: number
+}
